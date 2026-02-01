4785768d3ad4b1aba8ed54465181e3a64b60f162
Payal Gupta <prg17443@gmail.com>
2026-02-01 23:32:08 +0530

    Add solution for Recyclable and Low Fat Products

---
+++ sql/Day-01/01_1757_Recycleable_and_Low_Fat_Products.sql
@@ -0,0 +1,9 @@
+-- Leetcode SQL 50
+-- Question 1. 1757 Recyclable and Low Fat Products;
+-- https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50;
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+
+SELECT product_id 
+            FROM Products 
+                   WHERE recyclable = "Y" 
+                        AND low_fats = "Y";
