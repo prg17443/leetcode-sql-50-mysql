eab84d9e0eb5706fb14fb0e4951658ed7644cda1
prg17443 <prg17443@gmail.com>
2026-02-02 11:35:34 +0530

    Differentiating the question

--- sql/Day-01/01_1757_Recycleable_and_Low_Fat_Products.sql
+++ sql/Day-01/01_1757_Recycleable_and_Low_Fat_Products.sql
@@ -1,4 +1,5 @@
 -- Leetcode SQL 50
+-- ----SELECT---- --
 -- Question 1. 1757 Recyclable and Low Fat Products;
 -- https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50;
 -- Write a solution to find the ids of products that are both low fat and recyclable.
