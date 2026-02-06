c0b3964fb40c6e772fda4d0f5a2542c56c6c83fb
Payal Gupta <prg17443@gmail.com>
2026-02-06 18:59:50 +0530

    Add solution for User Activity for the Past 30 Days I

---
+++ sql/Day-06/24_1141_User_Activity_for_the_Past_30_Days_I.sql
@@ -0,0 +1,12 @@
+-- Question 24 : 1141. User Activity for the Past 30 Days I
+-- https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+-- Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+-- Key Point : Filter last 30 days --> GROUP BY activity_date --> COUNT(DISTINCT user_id) per day.
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM
+    Activity
+WHERE
+    activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date;
