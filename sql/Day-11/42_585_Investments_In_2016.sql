178bcc678c37f12705635da37b370bcf45255fde
Payal Gupta <prg17443@gmail.com>
2026-02-11 12:10:45 +0530

    Add solution for Investments in 2016

---
+++ sql/Day-11/42_585_Investments_In_2016.sql
@@ -0,0 +1,22 @@
+-- Question 42 : 585. Investments in 2016
+-- https://leetcode.com/problems/investments-in-2016/?envType=study-plan-v2&envId=top-sql-50
+-- Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who: have the same tiv_2015 value as one or more other policyholders, and
+-- are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique). Round tiv_2016 to two decimal places.
+-- Key Point : Duplicate 2015 value + Unique location → Sum 2016.
+SELECT 
+    ROUND(SUM(tiv_2016), 2) AS tiv_2016
+FROM
+    Insurance
+WHERE
+    tiv_2015 IN (SELECT 
+            tiv_2015
+        FROM
+            Insurance
+        GROUP BY tiv_2015
+        HAVING COUNT(*) > 1)
+        AND (lat , lon) IN (SELECT 
+            lat, lon
+        FROM
+            Insurance
+        GROUP BY lat , lon
+        HAVING COUNT(*) = 1);
